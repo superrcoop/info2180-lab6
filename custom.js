bcfc669fa0d0a5c6acf44e3c3778bc874d51a06c
Jordan cooper <jordan.cooper@mymona.uwi.edu>
2017-11-01 21:32:13 +0000

    added XML data and parsing w/ custom js

--- custom.js
+++ custom.js
@@ -1,15 +1,27 @@
 function search(){
-            var xhttp= new XMLHttpRequest();
-            var search=document.getElementById("search").value;
-            xhttp.onreadystatechange = function(){
-                if(this.readyState == 4 && this.status== 200){
-                    if(search){
-                       document.getElementById("result").innerHTML=this.responseText;
-                    }else{
-                       alert("Error: You must enter a word");
-                    }
-                }
+    var xhttp= new XMLHttpRequest();
+    var search=document.getElementById("search").value;
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status== 200){
+            if(search){
+                document.getElementById("result").innerHTML=this.responseText;
+            }else{
+                alert("Error: You must enter a word");
             }
-            xhttp.open("GET","request.php?q="+search,true);
-            xhttp.send();
         }
+    }
+    xhttp.open("GET","request.php?q="+search,true);
+    xhttp.send();
+}
+
+function searchall(){
+    var xhttp= new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status== 200){
+            var i;
+            var xml_dictionary=this.responseXML;
+        }
+    }
+    xhttp.open("GET","request.php?q=xmldata",true);
+    xhttp.send();
+} 
--- request.php
+++ request.php
@@ -16,3 +16,38 @@ $definition = [
 
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<dictionary>
+    <entry>
+        <word>definition</word>
+        <definition>A statement of the exact meaning of a word, especially in a dictionary.</definition>
+    </entry>
+    <entry>
+        <word>bar</word>
+        <definition>A place that sells alcholic beverages</definition>
+    </entry>
+    <entry>
+        <word>ajax</word>
+        <definition>Technique which involves the use of javascript and xml (or JSON)</definition>
+    </entry>
+    <entry>
+        <word>html</word>
+        <definition>The standard markup language for creating web pages and web applications.</definition>
+    </entry>
+    <entry>
+        <word>css</word>
+        <definition>A style sheet language used for describing the presentation of a document written in a markup language.</definition>
+    </entry>
+    <entry>
+        <word>javascript</word>
+        <definition>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</definition>
+    </entry>
+    <entry>
+        <word>php</word>
+        <definition>A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </entry>
+</dictionary>';
+
+$xml=simplexml_load_string($xmldata) or die("Error");
+print_r($xml);
